cdc7e013c6fdc19f4595518c6a6b2e61dc55ad34
handriss <andrashinkel@gmail.com>
2016-09-15 16:43:43 +0200

    xls and xlsx file uploads working

--- app/mod_xls/controllers.py
+++ app/mod_xls/controllers.py
@@ -1,3 +1,45 @@
 from flask import Blueprint
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 mod_xls = Blueprint('xls', __name__, url_prefix='/xls')
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + "/uploads"
+ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@mod_xls.route('/valami', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "műxik"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''

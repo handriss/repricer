c709fb9d2fcf69018d4e659ee2e854e76807302f
handriss <andrashinkel@gmail.com>
2016-09-18 18:44:33 +0200

    Excel data read into a dictionary

--- app/mod_xls/processer.py
+++ app/mod_xls/processer.py
@@ -5,8 +5,15 @@ from xlrd.sheet import ctype_text
 class FileProcesser():
 
     def __init__(self, route):
-        self.route = route
-        self.workbook = xlrd.open_workbook(self.route)
-        self.sheet_names = self.workbook.sheet_names()
-        print(self.workbook.sheet_by_index(0))
-        
+        book = xlrd.open_workbook(route)
+        sheet = book.sheet_by_index(0)
+        keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
+
+        dict_list = []
+        for row in range(1, sheet.nrows):
+            current_row = {}
+            for column in range(sheet.ncols):
+                current_row[sheet.cell(0, column).value] = sheet.cell(row, column).value
+            dict_list.append(current_row)
+
+        print(dict_list)

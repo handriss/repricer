da61c180083ed7fb871cb094ff4b739edab7503d
handriss <andrashinkel@gmail.com>
2016-09-20 16:49:56 +0200

    header and footer moved into one file

--- app/mod_bulk/controllers.py
+++ app/mod_bulk/controllers.py
@@ -6,5 +6,4 @@ mod_bulk = Blueprint('bulk', __name__, url_prefix='/bulk')
 
 @mod_bulk.route('/main/', methods=['GET'])
 def signin():
-    print("cica")
-    return render_template("bulk/repricer.html")
+    return render_template("index.html")

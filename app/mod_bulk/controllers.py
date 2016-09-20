6fcfc6ff9aefdb0868fae516c0420926f77e20f4
handriss <andrashinkel@gmail.com>
2016-09-20 20:13:05 +0200

    header and footer redone based on jinja docs

--- app/mod_bulk/controllers.py
+++ app/mod_bulk/controllers.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
+from peewee import *
 from app import db
+from app.mod_xls.models import Book, BaseModel
 
 mod_bulk = Blueprint('bulk', __name__, url_prefix='/bulk')
 
 
 @mod_bulk.route('/main/', methods=['GET'])
 def signin():
-    return render_template("index.html")
+    print("mukodj")
+    books = Book.select()
+    my_list = [1, 3, 235, 4]
+    for book in books:
+        print(book.author)
+    return render_template("index.html", books=my_list)

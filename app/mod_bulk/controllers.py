44e51248b09a96e5f7915e1d9354522965fbb97a
handriss <andrashinkel@gmail.com>
2016-09-20 09:59:37 +0200

    bulk folder added in templates

--- app/mod_bulk/controllers.py
+++ app/mod_bulk/controllers.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app import db
 
 mod_bulk = Blueprint('bulk', __name__, url_prefix='/')
+
+
+@mod_bulk.route('/', methods=['GET', 'POST'])
+def signin():
+
+    return render_template("bulk/index.html")
